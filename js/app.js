cd219d76dcaedc7c4ed85f6251c02d36c8e06445
Srinivas Nallapati <nals@amazon.com>
2019-05-10 08:59:03 -0700

    updated stats pages for user list

--- js/app.js
+++ js/app.js
@@ -1,36 +1,18 @@
 /* eslint-disable eol-last */
 /* eslint-disable no-unused-vars */
 'use strict';
-function showGame() {
-  document.getElementById('stats').style.display = 'none';
-  document.getElementById('options').style.display = 'none';
-  document.getElementById('game').style.display = 'block';
-}
-
-function showOptions() {
-  document.getElementById('game').style.display = 'none';
-  document.getElementById('stats').style.display = 'none';
-  document.getElementById('options').style.display = 'block';
-}
-
-function showStats() {
-  document.getElementById('game').style.display = 'none';
-  document.getElementById('options').style.display = 'none';
-  document.getElementById('stats').style.display = 'block';
-}
 
 function check() {
   window.scrollTo( 0, 0 );
   //document.getElementById('displayName').display='block';
   //document.getElementById('displayBalance').display='block';
-  
   var selectId = document.getElementById('userList');
   selectId.innerHTML = '';
   var defaultOption = document.createElement('option');
   defaultOption.text ='Selected';
   defaultOption.selected=true;
   selectId.add(defaultOption);
-  
+
   var userNamesArray = UserStore.getAllUserNames();
   console.log(userNamesArray.length);
   for (var i = 0; i < userNamesArray.length; i++) {
@@ -44,7 +26,7 @@ function check() {
 function createNewuser(event) {
   event.preventDefault();
   var newUserName = event.target.fname.value;
- 
+
   var userObj = new User(newUserName);
   UserStore.saveUser(userObj);
   newuserFormId.reset();
@@ -53,34 +35,28 @@ function createNewuser(event) {
 }
 
 function selectUser(event) {
-  console.log("in Select user");
+  console.log('in Select user');
   event.preventDefault();
   var e = document.getElementById('userList');
   var strUser = e.options[e.selectedIndex].value;
-  
   var currentUser = UserStore.setAndLoadCurrentUserByName(strUser);
-
   refreshUserDetailsOptionsPage();
   window.location = 'index.html';
-
-
-  // var displayNameId = document.getElementById('displayName');
-  // displayNameId.innerHTML = '<h1>Welcome '+ strUser+'!</h1>';
-  // displayNameId.style.display = 'block';
-  // document.getElementById('displayBalance').style.display = 'block';
-
-  // var displayBalanceElement = document.getElementById('displayBalance');
-  // displayBalanceElement.innerHTML = '<h1>Balance '+ currentUser.getAmount()+'!</h1>';
-  // displayBalanceElement.style.display = 'block';
 }
 
-var newuserFormId = document.getElementById('newuser_form');
-var selectuserFormId = document.getElementById('selectuser_form');
-newuserFormId.addEventListener('submit', createNewuser);
-selectuserFormId.addEventListener('submit', selectUser);
 
-var resetuserFormId = document.getElementById('resetuser_form');
-resetuserFormId.addEventListener('submit', resetUserBalance);
+function OnBootstrap() {
+  var newuserFormId = document.getElementById('newuser_form');
+  var selectuserFormId = document.getElementById('selectuser_form');
+  newuserFormId.addEventListener('submit', createNewuser);
+  selectuserFormId.addEventListener('submit', selectUser);
+
+  var resetuserFormId = document.getElementById('resetuser_form');
+  resetuserFormId.addEventListener('submit', resetUserBalance);
+
+}  
+OnBootstrap();
+
 
 function resetUserBalance(event) {
   console.log("in resetUserBalance user");
